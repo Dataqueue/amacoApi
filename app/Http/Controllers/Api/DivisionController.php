3747eee072c89499d3dc7241d011de9d976588da
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-02 14:33:26 +0530

    save

--- app/Http/Controllers/Api/DivisionController.php
+++ app/Http/Controllers/Api/DivisionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\division;
 use Illuminate\Http\Request;
 
 class DivisionController extends Controller
@@ -10,6 +11,11 @@ class DivisionController extends Controller
     //
     public function show(Division $div)
     {
-        return $employee;
+        $data = [
+            'id' => $div->id,
+            'name' => $div->name,
+        ];
+
+        return response()->json($data, 200);
     }
 }
