d3ac12946087aa92d91ecac5b3a36ab09c0e5104
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-06 10:37:11 +0530

    save

--- app/Http/Controllers/Api/ReceiptController.php
+++ app/Http/Controllers/Api/ReceiptController.php
@@ -37,13 +37,13 @@ class ReceiptController extends Controller
         //     $filePath = $request->file('file')->move('');
         // }
 
-        $receipt = Receipt::create(["party_id",$request->party_id,
-        "payment_mode",$request->party_id,
-        // "file",$filePath,
-        "paid_amount",$request->paid_amount,
-        "paid_date",$request->paid_date,
-        "div_id",$request->div_id,
-        "bank_id",$request->bank_id,
+        $receipt = Receipt::create(["party_id" => $request->party_id,
+        "payment_mode" => $request->party_id,
+         "file" => $filePath,
+        "paid_amount" => $request->paid_amount,
+        "paid_date" => $request->paid_date,
+        "div_id" => $request->div_id,
+        "bank_id" => $request->bank_id,
     ]);
 
         return response()->json($request->party_id, 200);
--- database/migrations/2021_03_12_063148_create_receipts_table.php
+++ database/migrations/2021_03_12_063148_create_receipts_table.php
@@ -16,9 +16,9 @@ class CreateReceiptsTable extends Migration
         Schema::create('receipts', function (Blueprint $table) {
             $table->id();
             $table->string('receipt_no')->nullable();
-            // $table->unsignedBigInteger('party_id')->nullable();
-            // $table->unsignedBigInteger('div_id')->nullable();
-            // $table->unsignedBigInteger('bank_id')->nullable();
+            $table->unsignedBigInteger('party_id')->nullable();
+            $table->unsignedBigInteger('div_id')->nullable();
+            $table->unsignedBigInteger('bank_id')->nullable();
             $table->unsignedBigInteger('invoice_no')->nullable();
             $table->string('payment_mode')->nullable();
             $table->string('credit_note_no')->nullable();
@@ -28,6 +28,7 @@ class CreateReceiptsTable extends Migration
             $table->string('paid_date')->nullable();
             $table->string('transaction_type')->nullable();
             $table->string('status')->nullable();
+            $table->string('file')->nullable();
             $table->timestamps();
         });
     }
