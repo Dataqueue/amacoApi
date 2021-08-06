64f3f6d3fe233898a0ef18e52b083dd061027303
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-06 11:38:30 +0530

    Update ReceiptController.php

--- app/Http/Controllers/Api/ReceiptController.php
+++ app/Http/Controllers/Api/ReceiptController.php
@@ -50,7 +50,7 @@ class ReceiptController extends Controller
     //     "bank_id" => $request->bank_id,
     // ]);
 
-        return response()->json($data['party_id'], 200);
+        return response()->json($request->file('file'), 200);
 
     }
 
