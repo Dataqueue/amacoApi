7822be02df74da1d3071b666967b3cefa27d47e8
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-10 13:23:37 +0530

    save

--- app/Http/Controllers/Api/ReceiptController.php
+++ app/Http/Controllers/Api/ReceiptController.php
@@ -78,7 +78,7 @@ class ReceiptController extends Controller
     public function update(Request $request, Receipt $receipt)
     {
         // $data = $request->json()->all();
-        $receipts = Receipt::findOrFail($request->id);
+        $receipt = Receipt::findOrFail($request->id);
         // $filePath=null;
         // if ($request->file('file')) {
         //     $filePath = $request->file('file')->move("receipts/", $request->file('file')->getClientOriginalName());
@@ -97,7 +97,7 @@ class ReceiptController extends Controller
             
         //     // 'contact_id' => $request->contact_id,
         // ]);
-        return response()->json($receipts);
+        return response()->json($receipt);
     }
 
     /**
