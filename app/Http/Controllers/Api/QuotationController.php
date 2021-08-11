846db677250d84b649dd3f4be134f75f16dbf00f
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-11 16:22:03 +0530

    save

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -464,6 +464,7 @@ class QuotationController extends Controller
                     'quantity' => $quotation_detail['quantity'],
                     'margin' => $quotation_detail['margin'],
                     'sell_price' => $quotation_detail['sell_price'],
+                    "amaco_description" => $quotation_detail['descriptionss'],
                     'remark' => $quotation_detail['remark'],
                     'file_img_url' => $filePath,
 
