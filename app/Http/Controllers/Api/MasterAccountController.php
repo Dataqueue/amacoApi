baf6b814c1b0340a9679fe980d8fa01f6f388461
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-12 15:10:48 +0530

    save`

--- app/Http/Controllers/Api/MasterAccountController.php
+++ app/Http/Controllers/Api/MasterAccountController.php
@@ -105,8 +105,6 @@ class MasterAccountController extends Controller
     {
         $invoiceCollection = new Collection();
         $date="2021-01-01";
-        $divEopenbalance=Floatval('0.00');
-        $divRopenbalance=Floatval('0.00');
         if($request->from_date){
             $invoiceCollection = Expense::join('payment_accounts','expenses.payment_account_id','payment_accounts.id')->join('divisions','expenses.div_id','divisions.id')->where('is_paid',1)->select('divisions.name as div_name','payment_accounts.name as nick_name','expenses.*')->whereBetween('expenses.created_at', [$request->from_date . ' ' . '00:00:00', $request->to_date ? $request->to_date . ' ' . '23:59:59' : now()])->get();
 
@@ -155,7 +153,7 @@ class MasterAccountController extends Controller
                 return [$item];
             }
         }));
-        $datas['opening_balance'] = $divRopenbalance;
+        $datas['opening_balance'] = $divEopenbalance;
         $datas['name'] = "All";
         $datas['from_date'] = $request['from_date'] ? $request['from_date'] : "2021-01-01";
         $datas['to_date'] = $request['to_date'] ? $request['to_date'] : substr(now(), 0, 10);
