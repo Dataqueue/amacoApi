bc1833ccb37e866a497d8d630d0f14f9dab61319
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-14 19:17:18 +0530

    Update QuotationController.php

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -555,7 +555,7 @@ class QuotationController extends Controller
         // new validation logic for po_number
 
         $unique_po_no = Quotation::where('po_number', $request->po_number)->first();
-        $data = $request->all();
+        $data = $request->json()->all();
         $quotation = Quotation::where("id", $id)->firstOrFail();
         $filePath = null;
         if ($request->file('file')) {
@@ -568,20 +568,18 @@ class QuotationController extends Controller
             }
 
             $data['sales_order_number'] = $this->getSalesOrderNumber();
-            // $quotation->update([
-            //     'status' => $data->status,
-            //     'sales_order_number' => $data['sales_order_number'],
-            //     'po_number' => $data->po_number,
-            //     'file' => $filePath,
+            $quotation->update([
+                'status' => $data->status,
+                'sales_order_number' => $data['sales_order_number'],
+                'po_number' => $data->po_number,
+                'file' => $filePath,
                 
-            // ]);
-            return response()->json($data);
+            ]);
         } else {
-            // $quotation->update([
-            //     'status' => $request->status,
-            //     'file' => $filePath,
-            // ]);
-            return response()->json($data);
+            $quotation->update([
+                'status' => $request->status,
+                'file' => $filePath,
+            ]);
         }
 
 
