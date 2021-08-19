741667998b65ade837b9cf7138eef4dd032f8faa
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-19 12:53:09 +0530

    saveing

--- routes/api.php
+++ routes/api.php
@@ -160,5 +160,6 @@ Route::post('updateQuotestatus', [QuotationController::class, 'updateQuotestatus
 Route::post('updateAdvancePay', [AdvancePaymentController::class, 'updateAdvancepay']);
 Route::post('masterstatement', [MasterAccountController::class, 'masterStatement']);
 Route::post('all-account-masterstatement', [MasterAccountController::class, 'allAccountmasterStatement']);
+Route::post('all-account', [MasterAccountController::class, 'allAccountmasterStatement']);
 
 
