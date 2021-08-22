6395634b9d1de97cfe1dfdc821fc1c2beb372865
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-22 08:25:04 +0530

    Update config.php

--- php_file/config.php
+++ php_file/config.php
@@ -11,7 +11,7 @@ session_start();
 //=========== database connection variables ====================
 define('DB_SERVER', "127.0.0.1"); // database host name eg. localhost or 127.0.0.1
 define('DB_USER', "root"); // database user name eg. root
-define('DB_DATABASE', "amaco"); //database name
+define('DB_DATABASE', "amacotest"); //database name
 define('DB_PASSWORD', "decoresite@12345"); //database user password      decoresite@12345
 define('DB_TYPE', 'mysql'); //database drive eg. mysql, pgsql, mongodb etc
 
