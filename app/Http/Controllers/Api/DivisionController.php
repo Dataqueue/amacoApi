6eeb33c2d0edba00196d7f2bf789bae9e42dee42
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-25 11:41:24 +0530

    save

--- app/Http/Controllers/Api/DivisionController.php
+++ app/Http/Controllers/Api/DivisionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Division;
 use App\Models\Expense;
 use App\Models\Receipt;
+use App\Models\PaymentAccount;
 use Illuminate\Http\Request;
 
 class DivisionController extends Controller
@@ -61,15 +62,31 @@ class DivisionController extends Controller
        
         $divEopenbalance=Expense::where('is_paid',1)->sum('amount');
         $divRopenbalance=Receipt::sum('paid_amount');
-        $division = Division::get();
+        $division = PaymentAccount::get();
         $datas=$division->map(function ($item) {
-            if($item['id'])
+            if($item['div_id'])
             {
-                $divEopenbalance=Expense::where('is_paid',1)->where('div_id',$item['id'])->sum('amount'); 
-                $divRopenbalance=Receipt::where('div_id',$item['id'])->sum('paid_amount');
-                $item['div_name']=$item->div_name;
+                $divEopenbalance=Expense::where('is_paid',1)->where('div_id',$item['div_id'])->sum('amount'); 
+                $divRopenbalance=Receipt::where('div_id',$item['div_id'])->sum('paid_amount');
+                $item['name']=$item->name;
                 $item['id']=$item->id;
                 $item['balance'] = $divRopenbalance-$divEopenbalance+floatval($item->opening_bal);
+                return $item;
+            }
+            if($item['bank_id'])
+            {
+               
+                $item['name']=$item->name;
+                $item['id']=$item->id;
+                $item['balance'] = $item->balance;
+                return $item;
+            }
+            if($item['user_id'])
+            {
+               
+                $item['name']=$item->name;
+                $item['id']=$item->id;
+                $item['balance'] = 0.00;
                 return $item;
             }
         
