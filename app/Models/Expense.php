0beeddf0b5ca24eb961d429b7d21cf6830a8be4c
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-28 11:05:06 +0530

    save

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -10,7 +10,7 @@ class Expense extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['payment_account_id' => 'array'];
+    protected $guarded = [];
 
     public function employee()
     {
