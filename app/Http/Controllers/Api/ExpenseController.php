f37b7bfc1c087983d0581692b2392be8ff4f794e
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-28 14:29:14 +0530

    Update ExpenseController.php

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -190,7 +190,7 @@ return response()->json($expenses);
                 }
                 return $item->column;
             }),
-            $expense->payment_account_id;
+            $expense->payment_account_id,
             // $expense->payment_account_id->map(
             //     function($items) use($expense) {
             //         $data=payment_accounts::where('id',$expense->id)->select('payment_accounts*');
