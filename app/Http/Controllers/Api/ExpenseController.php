ee84811a1a8e1cc7712ac5d5eb1e9e370e8137fe
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-28 14:00:07 +0530

    Update ExpenseController.php

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -36,10 +36,9 @@ class ExpenseController extends Controller
 //         });
 //         return response()->json($expenses);
 
-$expenses = Expense::join('account_categories','expenses.account_category_id','account_categories.id')->join('payment_accounts','expenses.payment_account_id','payment_accounts.id')->
-join('payment_accounts as payment_accountstable','payment_accounts.div_id','divtable.id')->select(
+$expenses = Expense::join('account_categories','expenses.account_category_id','account_categories.id')->join('payment_accounts','expenses.payment_account_id','payment_accounts.id')->select(
 'payment_accounts.name as paid_from',
-'payment_accountstable.name as paid_towards',
+'payment_accounts.name as paid_towards',
 'account_categories.name',
     'expenses.*'
 )->where("status", "new")->orderBy('created_at', 'DESC')->get();
