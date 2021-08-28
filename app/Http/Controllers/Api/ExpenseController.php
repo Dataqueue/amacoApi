742f8551d9e656027e1da779e5f76896c82d3d4d
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-28 11:51:26 +0530

    Update ExpenseController.php

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -121,7 +121,7 @@ return response()->json($expenses);
                 'payment_type' => $request->payment_type,
                 'check_no' => $request->cheque_no,
                 'transaction_id' => $request->transaction_id,
-                'payment_account_id' =>explode(',',array($demo)),
+                'payment_account_id' =>explode(',',$demo),
                 'description' => $request->description?$request->description:' ',
                 'referrence_bill_no' => $request->referrence_bill_no,
                 'tax' => $request->tax,
