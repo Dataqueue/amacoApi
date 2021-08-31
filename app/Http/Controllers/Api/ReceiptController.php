6f9e75f3bf4afb789c40225aec47bd74ff85a751
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-31 13:02:19 +0530

    Update ReceiptController.php

--- app/Http/Controllers/Api/ReceiptController.php
+++ app/Http/Controllers/Api/ReceiptController.php
@@ -45,20 +45,20 @@ class ReceiptController extends Controller
 
            
         }
-        $recieved_id=paymentAccount::where('div_id',$request->div_id)->get();
-    //     $receipt = Receipt::create(["party_id" => $request->party_id,
-    //     "payment_mode" => $request->payment_mode,
-    //     "narration" => $request->narration,
-    //     "file" => $filePath,
-    //     "paid_amount" => $request->paid_amount,
-    //     "paid_date" => $request->paid_date,
-    //     "div_id" => $request->div_id,
-    //     "bank_id" => $request->bank_id,
-    //     "sender" => $request->sender,
-    //     "receiver" => $request->receiver?$request->receiver:$recieved_id['div_id'],
-    // ]);
+        $recieved_id=paymentAccount::where('div_id',$request->div_id)->select('id')->get();
+        $receipt = Receipt::create(["party_id" => $request->party_id,
+        "payment_mode" => $request->payment_mode,
+        "narration" => $request->narration,
+        "file" => $filePath,
+        "paid_amount" => $request->paid_amount,
+        "paid_date" => $request->paid_date,
+        "div_id" => $request->div_id,
+        "bank_id" => $request->bank_id,
+        "sender" => $request->sender,
+        "receiver" => $request->receiver,
+    ]);
 
-        return response()->json($recieved_id->id, 200);
+        return response()->json($recieved_id, 200);
 
     }
 
