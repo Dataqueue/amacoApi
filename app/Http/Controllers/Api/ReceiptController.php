6374b1cb70cae82cb96142d520c40de4f5971ef6
Dataqueue <deekshitha.cse5235@gmail.com>
2021-08-31 13:29:45 +0530

    Update ReceiptController.php

--- app/Http/Controllers/Api/ReceiptController.php
+++ app/Http/Controllers/Api/ReceiptController.php
@@ -45,7 +45,7 @@ class ReceiptController extends Controller
 
            
         }
-        $recieved_id=paymentAccount::where('div_id',$request->div_id)->select('id')->get();
+        $recieved_id=paymentAccount::where('div_id',$request->div_id)->select('id')->get()->first();
         $id=$recieved_id['id'];
     //     $receipt = Receipt::create(["party_id" => $request->party_id,
     //     "payment_mode" => $request->payment_mode,
