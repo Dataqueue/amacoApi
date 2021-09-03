218c81dc43b70c925b4049b1bac60ae4473c6812
Dataqueue <deekshitha.cse5235@gmail.com>
2021-09-03 13:12:56 +0530

    Update MasterAccountController.php

--- app/Http/Controllers/Api/MasterAccountController.php
+++ app/Http/Controllers/Api/MasterAccountController.php
@@ -134,7 +134,7 @@ class MasterAccountController extends Controller
             if ($item->amount) {
                 $item['div_name']=$item->div_name;
                 $item['user_name']=$item->nick_name;
-                $item['div_id']=$item->utilized_div_id;
+                $item['div_id']=56;
                 $item['date'] = $item->created_at;
                 $item['code_no'] = $item->invoice_no;
                 $item['description'] = $item->description;
