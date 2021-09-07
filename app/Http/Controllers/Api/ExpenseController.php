0fca1e7c92fb5d108f71955a90b2fae4a6ee914e
Dataqueue <deekshitha.cse5235@gmail.com>
2021-09-07 15:23:37 +0530

    Update ExpenseController.php

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -339,7 +339,7 @@ return response()->json($expenses);
         $sumVal=floatval(0);
         $status=false;
         $amountVal=$request->amount;
-        // $res=AdvancePayment::where('expense_id',$request->id)->delete();
+        $res=AdvancePayment::where('expense_id',$request->id)->delete();
 
         $map = $arr->map(
             function($items) use($request,$sumVal,$status,$amountVal) {
