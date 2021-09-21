36ed9ff0467eee5fbf0ed268635fef46a5f30984
Dataqueue <deekshitha.cse5235@gmail.com>
2021-09-21 12:46:35 +0530

    Update AdvancePaymentStatementController.php

--- app/Http/Controllers/Api/AdvancePaymentStatementController.php
+++ app/Http/Controllers/Api/AdvancePaymentStatementController.php
@@ -136,8 +136,7 @@ class AdvancePaymentStatementController extends Controller
         if ($request->from_date) {
             // $expenseCollection = Expense::join('payment_accounts','expenses.payment_account_id','payment_accounts.id')->where('expenses.status','verified')->whereBetween('expenses.created_at', [$request->from_date . ' ' . '00:00:00', $request->to_date ? $request->to_date . ' ' . '23:59:59' : now()])->select('payment_accounts.name as user_name','expenses.*')->get();
             // $advanceEopenbalance=Expense::where('created_at', '<=', $request->from_date. ' ' . '00:00:00')->sum(str_replace(",","",'amount'));
-             $expenseCollection = AdvancePayment::join('payment_accounts','payment_accounts.id','advance_payments.payment_account_id')->whereBetween('advance_payments.created_at', [$request->from_date . ' ' . '00:00:00', $request->to_date ? $request->to_date . ' ' . '23:59:59' : now()])->get();
-            $advanceEopenbalance=Expense::where('created_at', '<=', $request->from_date. ' ' . '00:00:00')->sum(str_replace(",","",'amount'));
+             
 
 
 
@@ -154,7 +153,8 @@ class AdvancePaymentStatementController extends Controller
             $advancePaymentCollection = AdvancePayment::all();
         }
 
-        $data = $expenseCollection->concat($advancePaymentCollection);
+        // $data = $expenseCollection->concat($advancePaymentCollection);
+        $data = $advancePaymentCollection;
         $data = $data->sortBy('created_at');
 
         $data && ($datas['data'] = $data->map(function ($item) {
