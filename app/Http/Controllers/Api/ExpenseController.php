fd62d20254fb959c1c685b848107141a7d4a5ea2
Dataqueue <deekshitha.cse5235@gmail.com>
2021-09-23 12:16:06 +0530

    Update ExpenseController.php

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -459,7 +459,7 @@ return response()->json($expenses);
         
         $tempArray = (array) json_decode($request->data, true);
             foreach ($tempArray as $column_data_) {
-                $res=Expense::where('id',$column_data->id)->delete();       
+                $res=Expense::where('id',$column_data['id'])->delete();       
             }
 
         return response()->json($res);
