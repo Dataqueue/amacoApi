e5781507040ea4c3072b380691bd54b1a343d388
Dataqueue <deekshitha.cse5235@gmail.com>
2021-09-28 17:50:16 +0530

    save

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -464,9 +464,9 @@ return response()->json($expenses);
         $tempArray = (array) json_decode($request->data, true);
             foreach ($tempArray as $column_data_) {
                 
-                $res=Expense::where('id',$column_data_['id'])->each->destroy(); 
+                $res=Expense::where('id',$column_data_['id'])->destroy(); 
                
-                $ress=AdvancePayment::where('expense_id',$column_data_['id'])->each->destroy();
+                $ress=AdvancePayment::where('expense_id',$column_data_['id'])->each->delete();
                 // return response($column_data_['id']);      
             }
             // return response($column_data_['id']);
