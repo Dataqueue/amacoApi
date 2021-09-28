18195488b27e89318b753a492143560071b2b7c4
Dataqueue <deekshitha.cse5235@gmail.com>
2021-09-28 12:38:25 +0530

    save

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -403,7 +403,7 @@ return response()->json($expenses);
             'description' => $request->description?$request->description:" ",
             // 'referrence_bill_no' => $request->referrence_bill_no,
             'tax' => $request->tax,
-            'status' => $request->status,
+            // 'status' => $request->status,
             // 'paid_by' => $request->payment_account_id?$request->payment_account_id:null,
             'bank_ref_no' => $request->bank_ref_no,
             // 'bank_slip' => $request->file('bank_slip') ? $bank_slip_path : null,
