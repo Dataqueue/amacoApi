61b6217f2a41772836724639441d5da14deafafe
Dataqueue <deekshitha.cse5235@gmail.com>
2021-09-30 10:21:10 +0530

    Update ExpenseController.php

--- app/Http/Controllers/Api/ExpenseController.php
+++ app/Http/Controllers/Api/ExpenseController.php
@@ -471,8 +471,8 @@ return response()->json($expenses);
         //     $data['id']=$person['id'];
         //     return $data['id'];
         // });
-        // $demo=$arr2->toArray();
-        // $test=implode(',',$demo);
+        $demo=$arr2->toArray();
+        $test=implode(',',$demo);
             // foreach ($tempArray as $column_data_) {
                 
             //     // $res=Expense::where('id',$column_data_['id'])->destroy(); 
@@ -481,7 +481,7 @@ return response()->json($expenses);
             //     // return response($column_data_['id']);      
             // }
            
-            return response()->json($arr);
+            return response()->json($test);
         }
         if($request->status=="verify")
         {
