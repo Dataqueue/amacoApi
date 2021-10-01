615eab93fab77c28817015836efb905e265e6e31
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-01 16:06:42 +0530

    Update AccountStatementController.php

--- app/Http/Controllers/Api/AccountStatementController.php
+++ app/Http/Controllers/Api/AccountStatementController.php
@@ -133,7 +133,7 @@ class AccountStatementController extends Controller
                 $item['debit'] = floatval(str_replace(",","",$item->total_value));
                 $item['po_number'] = $item->po_number;
                 $item['credit'] = null;
-                $item['credit_days'] = floatval($item->credit_days);
+                $item['credit_days'] = floatval($item->party->credit_days);
                 return [$item];
             }
 
@@ -144,7 +144,7 @@ class AccountStatementController extends Controller
                 $item['credit'] = floatval(str_replace(",","",$item->paid_amount));
                 $item['po_number'] = $item->po_number;
                 $item['debit'] = null;
-                $item['credit_days'] = floatval($item->credit_days);
+                $item['credit_days'] = floatval($item->party->credit_days);
                 return [$item];
             }
         }));
