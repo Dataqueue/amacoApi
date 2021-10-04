03df0e5e713f46aed8691038d2fd7eda2275963e
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-04 14:54:27 +0530

    Update PartyController.php

--- app/Http/Controllers/Api/PartyController.php
+++ app/Http/Controllers/Api/PartyController.php
@@ -41,7 +41,7 @@ class PartyController extends Controller
     public function store(Request $request)
     {
         // return $request;
-        $val=$_ENV['url'];
+        $val=$_ENV['example.url'];
         // $json = json_decode(file_get_contents('https://translation.googleapis.com/language/translate/v2?key='.$val.'s&q=%22hello%22&target=ar'));
         // $translated_text = $json->responseData->translatedText;
 
