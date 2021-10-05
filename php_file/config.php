94f998fd5c8e4478c6335b41d38fc8a78c337cbc
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-05 10:32:32 +0530

    Update config.php

--- php_file/config.php
+++ php_file/config.php
@@ -5,15 +5,25 @@
  * Date: todays date
  * Time: todays time
  */
+$path = storage_path() . "/json/jsondata.json";
 
+$json = json_decode(file_get_contents($path), true);
 ob_start();
 session_start();
 //=========== database connection variables ====================
-define('DB_SERVER', "127.0.0.1"); // database host name eg. localhost or 127.0.0.1
-define('DB_USER', "root"); // database user name eg. root
-define('DB_DATABASE', "amacotest"); //database name
-define('DB_PASSWORD', "decoresite@12345"); //database user password      decoresite@12345
+// define('DB_SERVER', "127.0.0.1"); // database host name eg. localhost or 127.0.0.1
+// define('DB_USER', "root"); // database user name eg. root
+// define('DB_DATABASE', "amacotest"); //database name
+// define('DB_PASSWORD', "decoresite@12345"); //database user password      decoresite@12345
+// define('DB_TYPE', 'mysql'); //database drive eg. mysql, pgsql, mongodb etc
+
+
+define('DB_SERVER', $json['DB_SERVER']); // database host name eg. localhost or 127.0.0.1
+define('DB_USER', $json['DB_USER']); // database user name eg. root
+define('DB_DATABASE',$json['DB_DATABASE']); //database name
+define('DB_PASSWORD',$json['DB_PASSWORD']); //database user password      decoresite@12345
 define('DB_TYPE', 'mysql'); //database drive eg. mysql, pgsql, mongodb etc
+
 
 //========== site details described here ========================
 define('SITE_TITLE', 'Veidya.com');
