e94e0b1670c2b62e813e24f7ef6197c406dc2c21
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-07 16:00:55 +0530

    Update Expense.php

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -61,7 +61,7 @@ class Expense extends Model
     }
     public function accountcategory()
     {
-        return $this->hasMany(AccountCategory::class,'account_category_id','id');
+        return $this->hasMany(AccountCategory::class);
     }
 }
 
