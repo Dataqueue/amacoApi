f07eabb9700dc57b2330acd34120d3e4e93fcf29
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-13 17:41:37 +0530

    Update QuotationController.php

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -272,7 +272,7 @@ class QuotationController extends Controller
             }
 
             $quotation = Quotation::create($datas);
-            foreach (json_decode($request['notes']) as $div) {
+            foreach ($request->notes as $div) {
                    
                 $contact = notes::create([
                     'quotation_id' => $quotation->id,
