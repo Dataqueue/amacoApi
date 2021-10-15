a15ac26869263ac94879ce9e10fd00d911aed839
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-15 10:48:19 +0530

    Update QuotationController.php

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -283,7 +283,7 @@ class QuotationController extends Controller
                    
                 notes::create([
                     'quotation_id' => $quotation_id,
-                    'note' => $div->note, 
+                    'note' => 'save', 
                     
         
                 ]); 
