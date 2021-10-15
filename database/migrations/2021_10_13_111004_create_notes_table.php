b15f62444c455e38498298f69e7ce18d7ba77849
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-15 17:45:02 +0530

    Update 2021_10_13_111004_create_notes_table.php

--- database/migrations/2021_10_13_111004_create_notes_table.php
+++ database/migrations/2021_10_13_111004_create_notes_table.php
@@ -16,7 +16,7 @@ class CreateNotesTable extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('notes')->nullable();
+            $table->string('note')->nullable();
             $table->string('quotation_id')->nullable();
            
         });
