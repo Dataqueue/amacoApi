7364bc5dee42547920920d2c45364e44c243761f
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-26 15:18:26 +0530

    Update RFQController.php

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -105,10 +105,10 @@ class RFQController extends Controller
             $rfq_details = json_decode($request['rfq_details'], true);
             $index=0;
             foreach ($rfq_details as $rfq_detail) {
-                // if ($rfq_detail->file('file' . $index)) {
-                //     $filePath = $rfq_detail->file('file' . $index)->move('quotation/quotation_detail/' . $_rfq_id);
+                if ($rfq_detail->file('file' . $index)) {
+                    $filePath = $rfq_detail->file('file' . $index)->move('quotation/quotation_detail/' . $_rfq_id);
                    
-                // }
+                }
                 RFQDetails::create([
                     'product_id' => $rfq_detail['id'],
                     'description' => ucwords(trans($rfq_detail['descriptionss'])),
