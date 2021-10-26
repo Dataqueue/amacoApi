60a6face8bc9deee253ed0cf9c38ba0524a4a318
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-26 17:22:58 +0530

    Update RFQController.php

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -107,8 +107,8 @@ class RFQController extends Controller
             $index=0;
            
             foreach ($rfq_details as $rfq_detail) {
-                if ($request->file('file' . $index)) {
-                    $filePath = $request->file('file' . $index)->move('quotation/quotation_detail/' . $_rfq_id );
+                if ($rfq_detail->file('file' . $index)) {
+                    $filePath = $rfq_detail->file('file' . $index)->move('quotation/quotation_detail/' . $_rfq_id );
                 }
          
                 RFQDetails::create([
