e4498df3afc5ad3d6890dadbc6413ae2f9273aab
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-27 11:38:19 +0530

    Update RFQController.php

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -140,7 +140,7 @@ class RFQController extends Controller
             //     'rfq_id' => $_rfq_id,
             //     'file_name' => $res,
             // ]);
-            return response()->json([$data->file('file0')]);
+            return response()->json([$request->file('file0')->move('rfq/rfq_detail/' . $_rfq_id)]);
             // return ([
             //     'data' => $request->all(),
             // ]);
