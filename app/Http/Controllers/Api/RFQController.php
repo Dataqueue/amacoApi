bbe70b4d94fc306673eeae4c6b4909b80a34b714
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-27 10:59:23 +0530

    Update RFQController.php

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -108,8 +108,8 @@ class RFQController extends Controller
             foreach ($rfq_details as $rfq_detail) {
                 
                  $index = 0;
-                while ($request['rfq_details' . $index] != null) {
-                    $quotation_detail = (array) json_decode($request['rfq_details' . $index], true);
+                while ($request['rfq_details'] != null) {
+                    // $quotation_detail = (array) json_decode($request['rfq_details' . $index], true);
                     $filePath = null;
                     if ($request->file('file' . $index)) {
                         $filePath = $request->file('file' . $index)->move('rfq/rfq_detail/' . $_rfq_id);
