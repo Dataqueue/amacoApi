aa85b07e87e2f3052356606b781427c20603a491
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-27 15:44:51 +0530

    Update RFQController.php

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -200,7 +200,7 @@ class RFQController extends Controller
                     // "product_name" => $rfq_detail->product->name,
                     "product_name" => $rfq_detail->product_name,
                     "product" => array($rfq_detail->product),
-                    "prices" => isset($rfq_detail->product_id)?$rfq_detail->product->productPrice:null,
+                    "prices" => isset($rfq_detail->product_id)?$rfq_detail->product->productPrice:[],
                     "party" =>  isset($rfq_detail->product_id)?$rfq_detail->product->productPrice->map(function ($price) {
                         return ($price->party);
                     }):null,
