383615c2b5a1ce9bd719082f4b4d9fc83fe83002
Dataqueue <deekshitha.cse5235@gmail.com>
2021-10-27 10:33:17 +0530

    Update RFQController.php

--- app/Http/Controllers/Api/RFQController.php
+++ app/Http/Controllers/Api/RFQController.php
@@ -104,7 +104,7 @@ class RFQController extends Controller
 
             $rfq_details = json_decode($request['rfq_details'], true);
            
-            $index=0;
+           
            
             foreach ($rfq_details as $rfq_detail) {
                 
