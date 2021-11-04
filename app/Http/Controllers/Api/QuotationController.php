88b6b91a3b2ebf272a602f765a6757a707940010
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-04 16:10:10 +0530

    Update QuotationController.php

--- app/Http/Controllers/Api/QuotationController.php
+++ app/Http/Controllers/Api/QuotationController.php
@@ -296,7 +296,7 @@ class QuotationController extends Controller
                         'description' => $quotation_detail['product_id']?$quotation_detail['product_name']:$quotation_detail['product'],
                         'product_description' => $quotation_detail['description'],
                         'quantity' => $quotation_detail['quantity'],
-                        'unit_of_measure' => $quotation_detail['unit_of_measure'],
+                        'unit_of_measure' => $quotation_detail['unit_of_measure']?$quotation_detail['unit_of_measure']:" ",
                         'margin' => $quotation_detail['margin'],
                         'sell_price' => $quotation_detail['sell_price'],
                         'remark' => $quotation_detail['remark'],
