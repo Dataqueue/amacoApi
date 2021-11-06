4bbc823567f29d9cfdcbd58088f36c639aef71ce
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-06 12:12:31 +0530

    Update InvestmentsDetailsController.php

--- app/Http/Controllers/Api/InvestmentsDetailsController.php
+++ app/Http/Controllers/Api/InvestmentsDetailsController.php
@@ -14,21 +14,21 @@ class InvestmentsDetailsController extends Controller
     public function store(Request $request)
     {
      
-        $data=InvestmentsDetails::create([
-           'user_id'=>$request->user_id,
-           'payment_account_id'=>$request->id ,
-           'amount'=>$request->balance 
-        ]);
-        $pay=AdvancePayment::create([
-            'narration'=>'Investments',
-            'payment_account_id'=>$request->payment_account_id,
-            'amount'=>$request->balance,
-            'received_by'=>$request->received_by,
-            'payment_mode'=>'cash',
+        // $data=InvestmentsDetails::create([
+        //    'user_id'=>$request->user_id,
+        //    'payment_account_id'=>$request->id ,
+        //    'amount'=>$request->balance 
+        // ]);
+        // $pay=AdvancePayment::create([
+        //     'narration'=>'Investments',
+        //     'payment_account_id'=>$request->payment_account_id,
+        //     'amount'=>$request->balance,
+        //     'received_by'=>$request->received_by,
+        //     'payment_mode'=>'cash',
 
 
-        ]);
-        return response()->json($data);
+        // ]);
+        return response()->json($request);
     }
 }
 
