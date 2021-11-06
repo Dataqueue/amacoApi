cb4baf7ea01f85c75b0f64b500dfd70f43aeb9dc
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-06 12:06:34 +0530

    Update InvestmentsDetailsController.php

--- app/Http/Controllers/Api/InvestmentsDetailsController.php
+++ app/Http/Controllers/Api/InvestmentsDetailsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\InvestmentsDetail;
+use App\Models\InvestmentsDetails;
 use App\Models\AdvancePayment;
 use App\Models\PaymentAccount;
 
