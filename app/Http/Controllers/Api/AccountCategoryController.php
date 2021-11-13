7ab206e099151d81f94724e0fe07881baa891e36
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-13 11:00:33 +0530

    Update AccountCategoryController.php

--- app/Http/Controllers/Api/AccountCategoryController.php
+++ app/Http/Controllers/Api/AccountCategoryController.php
@@ -41,7 +41,8 @@ class AccountCategoryController extends Controller
         // dd($groupedCategories[0]);
         if($groupedCategories->parent_id!==""){
 
-            $temp=$this->checkParentcategories($groupedCategories->parent_id,$groupedCategories->id);
+            // $temp=$this->checkParentcategories($groupedCategories->parent_id,$groupedCategories->id);
+            return "hhhhhhh";
         }
         else{
             return $groupedCategories->name;
