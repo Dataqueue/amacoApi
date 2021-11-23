fdf19a75d6a7d1b56813d5214e0f43ab7856d6b0
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-23 16:33:26 +0530

    Update CompanyController.php

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -48,7 +48,7 @@ class CompanyController extends Controller
             $data['img3'] = $img3_path;
         }
         $apikey=  \Config::get('example.key');
-        $namear = json_decode(file_get_contents('https://translation.googleapis.com/language/translate/v2?key='.$apikey.'&q='.urlencode($request->name).'&target=ar'));
+        $namear = json_decode(file_get_contents('https://translation.googleapis.com/language/translate/v2?key='.$apikey.'&q='.urlencode("amaco").'&target=ar'));
         $company = Company::create([
             'name'=>$request['name'],
             'email'=>$request['email'],
