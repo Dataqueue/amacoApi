ba6d79f6e72eac92baeaf16fe71f8ade7be28c87
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-23 14:50:57 +0530

    Update api.php

--- app/Http/Controllers/Api/api.php
+++ app/Http/Controllers/Api/api.php
@@ -112,7 +112,7 @@ Route::apiResource('company-bank', CompanyBankController::class);
 
 // restful api links
 
-Route::post('updateCompany',[CompanyController::class, 'update'])->name('rfq.company');
+Route::post('update_Company',[CompanyController::class,'update']);
 Route::post('rfq-history', [RFQController::class, 'history'])->name('rfq.history');
 Route::post('invoice-history', [InvoiceController::class, 'history'])->name('invoice.history');
 Route::post('quotation-history', [QuotationController::class, 'history'])->name('quotation.history');
