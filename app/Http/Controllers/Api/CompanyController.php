a5795acbdf1f1207807194bf30274070c3600076
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-23 15:26:15 +0530

    Update CompanyController.php

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -87,7 +87,7 @@ class CompanyController extends Controller
 
                 File::delete(public_path($company->img1));
 
-                $company->update([
+                $id->update([
                     'img1' => null
                 ]);
             }
@@ -100,7 +100,7 @@ class CompanyController extends Controller
 
                 File::delete(public_path($company->img2));
 
-                $company->update([
+                $id->update([
                     'img2' => null
                 ]);
             }
@@ -134,7 +134,18 @@ class CompanyController extends Controller
 
 
         $data = $request->all();
-        $id=Company::where('id',$data['id'])->get();
+        $id=Company::where('id',$data['id'])->update([
+            'name'=>$request['name'],
+            'email'=>$request['email'],
+            'address'=>$request['address'],
+            'contact'=>$request['contact'],
+            'cr_no'=>$request['cr_no'],
+            'po_box'=>$request['po_box'],
+            'vat_no'=>$request['vat_no'],
+            'fax'=>$request['fax'],
+            'website'=>$request['website'],
+
+        ]);;
         if ($request->file('img1')) {
             if (File::exists(public_path($company->img1))) {
 
@@ -175,18 +186,7 @@ class CompanyController extends Controller
             $data['img3'] = $img3_path;
         }
 
-        $id->update([
-            'name'=>$request['name'],
-            'email'=>$request['email'],
-            'address'=>$request['address'],
-            'contact'=>$request['contact'],
-            'cr_no'=>$request['cr_no'],
-            'po_box'=>$request['po_box'],
-            'vat_no'=>$request['vat_no'],
-            'fax'=>$request['fax'],
-            'website'=>$request['website'],
-
-        ]);
+        
 
         return response()->json($id);
     }
