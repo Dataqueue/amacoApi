37757e74d7504038995c9a323db2312af1621ff6
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-24 10:49:14 +0530

    Update CompanyController.php

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -169,19 +169,19 @@ class CompanyController extends Controller
             'website'=>$request['website'],
 
         ]);;
-        // if ($request->file('img1')) {
-        //     if (File::exists(public_path($company->Img1()))) {
+        if ($request->file('img1')) {
+            // if (File::exists(public_path($company->Img1()))) {
 
-        //         File::delete(public_path($company->Img1()));
+            //     File::delete(public_path($company->Img1()));
 
-        //         $company->update([
-        //             'img1' => null
-        //         ]);
-        //     }
-        //     $img1_name = $request['img1']->getClientOriginalName();
-        //     $img1_path = $request->file('img1')->move('company/', $img1_name);
-        //     $data['img1'] = $img1_path;
-        // }
+            //     $company->update([
+            //         'img1' => null
+            //     ]);
+            // }
+            $img1_name = $request['img1']->getClientOriginalName();
+            $img1_path = $request->file('img1')->move('company/', $img1_name);
+            $data['img1'] = $img1_path;
+        }
         // if ($request->file('img2')) {
         //     if (File::exists(public_path($company->Img2()))) {
 
