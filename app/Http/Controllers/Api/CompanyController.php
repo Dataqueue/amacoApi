ae8b9c4616478eeeb90b8c922eeb604e039916c7
Dataqueue <deekshitha.cse5235@gmail.com>
2021-11-24 10:47:26 +0530

    Update CompanyController.php

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -169,45 +169,45 @@ class CompanyController extends Controller
             'website'=>$request['website'],
 
         ]);;
-        if ($request->file('img1')) {
-            if (File::exists(public_path($company->Img1()))) {
+        // if ($request->file('img1')) {
+        //     if (File::exists(public_path($company->Img1()))) {
 
-                File::delete(public_path($company->Img1()));
+        //         File::delete(public_path($company->Img1()));
 
-                $company->update([
-                    'img1' => null
-                ]);
-            }
-            $img1_name = $request['img1']->getClientOriginalName();
-            $img1_path = $request->file('img1')->move('company/', $img1_name);
-            $data['img1'] = $img1_path;
-        }
-        if ($request->file('img2')) {
-            if (File::exists(public_path($company->Img2()))) {
+        //         $company->update([
+        //             'img1' => null
+        //         ]);
+        //     }
+        //     $img1_name = $request['img1']->getClientOriginalName();
+        //     $img1_path = $request->file('img1')->move('company/', $img1_name);
+        //     $data['img1'] = $img1_path;
+        // }
+        // if ($request->file('img2')) {
+        //     if (File::exists(public_path($company->Img2()))) {
 
-                File::delete(public_path($company->Img2()));
+        //         File::delete(public_path($company->Img2()));
 
-                $company->update([
-                    'img2' => null
-                ]);
-            }
-            $img2_name = $request['img2']->getClientOriginalName();
-            $img2_path = $request->file('img2')->move('company/', $img2_name);
-            $data['img2'] = $img2_path;
-        }
-        if ($request->file('img3')) {
-            if (File::exists(public_path($company->Img3()))) {
+        //         $company->update([
+        //             'img2' => null
+        //         ]);
+        //     }
+        //     $img2_name = $request['img2']->getClientOriginalName();
+        //     $img2_path = $request->file('img2')->move('company/', $img2_name);
+        //     $data['img2'] = $img2_path;
+        // }
+        // if ($request->file('img3')) {
+        //     if (File::exists(public_path($company->Img3()))) {
 
-                File::delete(public_path($company->Img3()));
+        //         File::delete(public_path($company->Img3()));
 
-                $company->update([
-                    'img3' => null
-                ]);
-            }
-            $img3_name = $request['img3']->getClientOriginalName();
-            $img3_path = $request->file('img3')->move('company/', $img3_name);
-            $data['img3'] = $img3_path;
-        }
+        //         $company->update([
+        //             'img3' => null
+        //         ]);
+        //     }
+        //     $img3_name = $request['img3']->getClientOriginalName();
+        //     $img3_path = $request->file('img3')->move('company/', $img3_name);
+        //     $data['img3'] = $img3_path;
+        // }
 
         
 
