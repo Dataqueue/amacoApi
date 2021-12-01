af95cd85ab8638630b7fcf358442199de3c7ba46
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-01 13:23:55 +0530

    save

--- app/Http/Controllers/Api/ReceiptController.php
+++ app/Http/Controllers/Api/ReceiptController.php
@@ -179,7 +179,7 @@ class ReceiptController extends Controller
             'check_no' => $request->check_no,
             'bank_id' => $request->bank_id,
             "sender" => $request->sender,
-            "receiver" => $request->receiver!==null?$request->receiver:0,
+            // "receiver" => $request->receiver!==null?$request->receiver:0,
             "paid_date" => $request->paid_date,
             // 'file' => $filePath,
         
