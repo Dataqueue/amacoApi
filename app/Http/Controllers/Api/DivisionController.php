959c2729afdf711860a3ff9ee56818a0ee064ff8
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-03 11:51:09 +0530

    Update DivisionController.php

--- app/Http/Controllers/Api/DivisionController.php
+++ app/Http/Controllers/Api/DivisionController.php
@@ -101,7 +101,7 @@ class DivisionController extends Controller
             $recievedby=AdvancePayment::where('received_by',$item['id'])->sum('amount');
             $paidby=AdvancePayment::where('payment_account_id',$item['id'])->sum('amount');
             $paid_date=AdvancePayment::where('received_by',$item['id'])->orWhere('payment_account_id',$item['id'])->orderBy('created_at','DESC')->get('created_at');
-            $item['date']=$paid_date[0]->created_at;
+            $item['date']=$paid_date;
             $item['name']=$item->name;
             $item['id']=$item->id;
             $item['balance'] =$accountSum+$recievedby-$paidby;
