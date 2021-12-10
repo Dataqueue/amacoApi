7d84ddb26749c7a8b8ecfcf8de8e099b14bb790a
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-10 11:06:48 +0530

    Update AccountCategoryController.php

--- app/Http/Controllers/Api/AccountCategoryController.php
+++ app/Http/Controllers/Api/AccountCategoryController.php
@@ -165,10 +165,10 @@ class AccountCategoryController extends Controller
             $sub_categories = AccountCategory::where('parent_id', '=', null)->whereOr('delete_status',0)->get();
         }else{
             // $sub_categories = AccountCategory::where([['parent_id', '=', $id],['delete_status','=',1]])->get();
-            $sub_categories = AccountCategory::where('delete_status','=',1)->get();
+            $sub_categories = AccountCategory::where('delete_status',1)->get();
         }
         if($sub_categories){
-            return response()->json($sub_categories);
+            return response()->json($id);
         }
         return response()->json(null);
     }
