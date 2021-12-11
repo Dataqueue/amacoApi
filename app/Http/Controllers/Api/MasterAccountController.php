0398ac6f41f7ac5fd5552d7540c6652a22a4d56d
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-11 15:13:47 +0530

    Update MasterAccountController.php

--- app/Http/Controllers/Api/MasterAccountController.php
+++ app/Http/Controllers/Api/MasterAccountController.php
@@ -139,8 +139,8 @@ class MasterAccountController extends Controller
             return $obj;
         });
 
-        $data =$advance;
-        // $data = $invoiceCollection->concat($receiptCollection)->concat($advanceCollection);
+        // $data =$advance;
+        $data = $invoiceCollection->concat($receiptCollection)->concat($advance);
         
 
         $data && ($datas['data'] = $data->map(function ($item) {
@@ -178,8 +178,8 @@ class MasterAccountController extends Controller
             if($item->status=="advance_type")
             {
 
-                // if($item->payment_account->type=="division" && $item->received_by->type=="personal")
-                // {
+                if($item->payment_account->type=="division" && $item->received_by->type=="personal")
+                {
                     // $item['div_name']=$item->payment_account->name;
                     $item['date'] = $item->created_at;
                     $item['code_no'] = " ";
@@ -192,24 +192,24 @@ class MasterAccountController extends Controller
                     // $item['credit_days'] = floatval($item->credit_days);
                     return [$item];
 
-                // }
-                // if($item->received_by->type=="division" && $item->payment_account->type=="personal")
-                // {
+                }
+                if($item->received_by->type=="division" && $item->payment_account->type=="personal")
+                {
                     
-                //     if ($item->paid_amount) {
-                //     $item['div_name']=$item->received_by->name;
-                //     $item['date'] = $item->created_at;
-                //     $item['code_no'] = " ";
-                //     $item['paid_to'] = $item->payment_account->name;
-                //     $item['description'] = $item->narration;
-                //     $item['cat_name'] = 'Division';
-                //     $item['credit'] = floatval(str_replace(",","",$item->amount));
-                //     $item['po_number'] = " ";
-                //     $item['debit'] = null;
-                //         // $item['credit_days'] = floatval($item->credit_days);
-                //         return [$item];
-                // }
-            // }
+                    if ($item->paid_amount) {
+                    // $item['div_name']=$item->received_by->name;
+                    $item['date'] = $item->created_at;
+                    $item['code_no'] = " ";
+                    // $item['paid_to'] = $item->payment_account->name;
+                    $item['description'] = $item->narration;
+                    $item['cat_name'] = 'Division';
+                    $item['credit'] = floatval(str_replace(",","",$item->amount));
+                    $item['po_number'] = " ";
+                    $item['debit'] = null;
+                        // $item['credit_days'] = floatval($item->credit_days);
+                        return [$item];
+                }
+            }
 
 
             }
