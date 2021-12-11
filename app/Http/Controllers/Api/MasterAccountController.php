32dfdcf6d9047d549ba99450113f951e9c666625
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-11 17:58:22 +0530

    Update MasterAccountController.php

--- app/Http/Controllers/Api/MasterAccountController.php
+++ app/Http/Controllers/Api/MasterAccountController.php
@@ -149,38 +149,38 @@ class MasterAccountController extends Controller
         // $data = $invoiceCollection->concat($receiptCollection)->concat($advance);
         
 
-        $data && ($datas['data'] = $data->filter(function ($item) {
+        $data && ($datas['data'] = $data->map(function ($item) {
             // if ($item->amount) {
-            // if ($item->expense_type) {
-            //     $item['div_name']=$item->div_name;
-            //     $item['user_name']=$item->nick_name;
-            //     $item['div_id']=$item->divid;
-            //     $item['date'] = $item->created_at;
-            //     $item['code_no'] = $item->invoice_no;
-            //     $item['description'] = $item->description;
-            //     $item['paid_to'] = $item->paid_to;
-            //     $item['cat_name'] = $item->cat_name;
-            //     $item['credit'] = floatval(str_replace(",","",$item->amount));
-            //     $item['po_number'] = $item->po_number;
-            //     $item['debit'] = null;
-            //     // $item['credit_days'] = floatval($item->credit_days);
-            //     return [$item];
-            // }
+            if ($item->expense_type) {
+                $item['div_name']=$item->div_name;
+                $item['user_name']=$item->nick_name;
+                $item['div_id']=$item->divid;
+                $item['date'] = $item->created_at;
+                $item['code_no'] = $item->invoice_no;
+                $item['description'] = $item->description;
+                $item['paid_to'] = $item->paid_to;
+                $item['cat_name'] = $item->cat_name;
+                $item['credit'] = floatval(str_replace(",","",$item->amount));
+                $item['po_number'] = $item->po_number;
+                $item['debit'] = null;
+                // $item['credit_days'] = floatval($item->credit_days);
+                return [$item];
+            }
 
-            // if ($item->receipt_type) {
-            // // if ($item->paid_amount) {
-            //     $item['div_name']=$item->div_name;
-            //     $item['date'] = $item->created_at;
-            //     $item['code_no'] = $item->receipt_no;
-            //     $item['paid_to'] = $item->paid_to;
-            //     $item['description'] = $item->narration;
-            //     $item['cat_name'] = 'Received';
-            //     $item['debit'] = floatval(str_replace(",","",$item->paid_amount));
-            //     $item['po_number'] = $item->po_number;
-            //     $item['credit'] = null;
-            //     // $item['credit_days'] = floatval($item->credit_days);
-            //     return [$item];
-            // }
+            if ($item->receipt_type) {
+            // if ($item->paid_amount) {
+                $item['div_name']=$item->div_name;
+                $item['date'] = $item->created_at;
+                $item['code_no'] = $item->receipt_no;
+                $item['paid_to'] = $item->paid_to;
+                $item['description'] = $item->narration;
+                $item['cat_name'] = 'Received';
+                $item['debit'] = floatval(str_replace(",","",$item->paid_amount));
+                $item['po_number'] = $item->po_number;
+                $item['credit'] = null;
+                // $item['credit_days'] = floatval($item->credit_days);
+                return [$item];
+            }
             // if($item->status=="advance_type")
             // {
 
@@ -206,7 +206,7 @@ class MasterAccountController extends Controller
                     $item['div_name']=$item->receivedBy->name;
                     $item['date'] = $item->created_at;
                     $item['code_no'] = " ";
-                     $item['paid_to'] = $item->paymentAccount->name;
+                    $item['paid_to'] = $item->paymentAccount->name;
                     $item['description'] = $item->narration;
                     $item['cat_name'] = 'Division';
                     $item['credit'] = floatval(str_replace(",","",$item->amount));
