39a4ad8757d0e37915e58266ba5ae39066e26dd0
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-11 15:09:01 +0530

    Update MasterAccountController.php

--- app/Http/Controllers/Api/MasterAccountController.php
+++ app/Http/Controllers/Api/MasterAccountController.php
@@ -208,7 +208,7 @@ class MasterAccountController extends Controller
                 //         // $item['credit_days'] = floatval($item->credit_days);
                 //         return [$item];
                 // }
-            }
+            // }
 
 
             }
