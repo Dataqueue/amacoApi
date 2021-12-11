3bd80f5562ad73fbde4266ca8c5107b4ac8eaace
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-11 16:49:07 +0530

    Update MasterAccountController.php

--- app/Http/Controllers/Api/MasterAccountController.php
+++ app/Http/Controllers/Api/MasterAccountController.php
@@ -210,7 +210,7 @@ class MasterAccountController extends Controller
                 //     return [$item];
                 // }
 
-                    $item['dee']=$item[0];
+                    $item['dee']=$item->advance_type;
                 // }
                   
 
