be8a5bad213fd72599609963814230e25deec15f
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-11 15:18:45 +0530

    Update MasterAccountController.php

--- app/Http/Controllers/Api/MasterAccountController.php
+++ app/Http/Controllers/Api/MasterAccountController.php
@@ -133,8 +133,8 @@ class MasterAccountController extends Controller
         $advance=AdvancePayment::whereBetween('advance_payments.created_at', [$request->from_date . ' ' . '00:00:00', $request->to_date ? $request->to_date. ' ' . '23:59:59' : now()])->get();
         $advance=$advance->map(function($obj){
             $obj["status"]='advance_type';
-            $obj->paymentAccount;
-            $obj->receivedBy;
+            [$obj->paymentAccount];
+            [$obj->receivedBy];
 
             return $obj;
         });
