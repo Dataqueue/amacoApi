20acbf30012768d063bc9d83612f6926495be0d9
Dataqueue <deekshitha.cse5235@gmail.com>
2021-12-18 12:38:56 +0530

    Update StockController.php

--- app/Http/Controllers/Api/StockController.php
+++ app/Http/Controllers/Api/StockController.php
@@ -35,7 +35,7 @@ class StockController extends Controller
             $accountCategories->map(function($accountCategory){
             return [
                 'category' => $accountCategory,
-                'sub_categories' => $accountCategory->purchase_salesReturn,
+                'sub_categories' => $accountCategory->sales_purchase_Return,
                 // 'sub_categories' => $this->subCategory($accountCategory->id),
             ];
         }),
