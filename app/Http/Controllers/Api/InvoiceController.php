c51a52a02bae1e5c3ddcfc66c4c6f95381f4877d
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-01 13:05:21 +0530

    Update InvoiceController.php

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -161,7 +161,7 @@ class InvoiceController extends Controller
                     $invoice_detail['margin']=((((float)$invoice_detail->sell_price)-((float)$invoice_detail->purchase_price))/((float)
                     
                     $invoice_detail->purchase_price))*100,
-                    $invoice_detail['delivered_quantity']=0,
+                    $invoice_detail['delivered_quantity']=$invoice_detail->getDelivered_invoice_Quantity($invoice_detail),
                     $invoice_detail->quotationDetail,
                     $invoice_detail->product
                 ];
