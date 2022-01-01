8f50eb21fb70a4760beae54f0e4dcaa11a123ea3
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-01 15:56:50 +0530

    Update DeliveryNoteDetailController.php

--- app/Http/Controllers/Api/DeliveryNoteDetailController.php
+++ app/Http/Controllers/Api/DeliveryNoteDetailController.php
@@ -86,7 +86,7 @@ class DeliveryNoteDetailController extends Controller
         $totalDeliveredQuantity = $this->getTotalDeliveredQuantity($totalDeliveryNoteDetail);
 
         $data = [
-            "total_quantity"=>$totalQuantity = $quotationDetail,
+            "total_quantity"=>$quotationDetail,
             "total_delivered_quantity"=>$totalDeliveredQuantity,
             // 'balance_quantity' => $this->getBalanceQuantity($totalQuantity, $totalDeliveredQuantity),
             "delivery_notes_detail"=>$delivery_notes_detail,
