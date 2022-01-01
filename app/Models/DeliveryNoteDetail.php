7f31b3774abf82694ff2322cefe947d521135665
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-01 12:12:48 +0530

    Update DeliveryNoteDetail.php

--- app/Models/DeliveryNoteDetail.php
+++ app/Models/DeliveryNoteDetail.php
@@ -71,7 +71,7 @@ class DeliveryNoteDetail extends Model
         }
 
         // $totalDeliveredQuantity = $this->getTotalDeliveredQuantity($totalDeliveryNoteDetails);
-        $totalDeliveredQuantity =getTotalDeliveredQuantity($quotationDetail->$delivery_notes_detail);
+        $totalDeliveredQuantity =$this->getTotalDeliveredQuantity($quotationDetail->$delivery_notes_detail);
         // if(isset($totalDeliveredQuantity)){
         //     $totalDeliveredQuantityExceptCurrentValue = $totalDeliveredQuantity - intval($delivery_notes_detail->delivered_quantity) ;
         // }else{
