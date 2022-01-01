c89d1651c328d720ed691b4fd65af3b83496d185
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-01 11:46:13 +0530

    Update DeliveryNoteDetail.php

--- app/Models/DeliveryNoteDetail.php
+++ app/Models/DeliveryNoteDetail.php
@@ -47,47 +47,47 @@ class DeliveryNoteDetail extends Model
     {
         $delivery_notes_detail = DeliveryNoteDetail::where('id',$id)->first();
 
-        // $totalDeliveryNoteDetails = DeliveryNoteDetail::where([
-        //     'delivery_note_id' => $delivery_notes_detail->delivery_note_id,
-        //     'product_id' => $delivery_notes_detail->product_id,
-        // ])->get();
+        $totalDeliveryNoteDetails = DeliveryNoteDetail::where([
+            'delivery_note_id' => $delivery_notes_detail->delivery_note_id,
+            'product_id' => $delivery_notes_detail->product_id,
+        ])->get();
 
-        // if($delivery_notes_detail->quotation_id)
-        // {
-        // $quotationDetail = QuotationDetail::where([
-        //     'quotation_id' => $delivery_notes_detail->deliveryNote->quotation_id,
-        //     'product_id' => $delivery_notes_detail->product_id,
-        // ])->firstOrFail();
-        // }
-        // else{
+        if($delivery_notes_detail->quotation_id)
+        {
+        $quotationDetail = QuotationDetail::where([
+            'quotation_id' => $delivery_notes_detail->deliveryNote->quotation_id,
+            'product_id' => $delivery_notes_detail->product_id,
+        ])->firstOrFail();
+        }
+        else{
 
        
-        // $invoiceDetail = InvoiceDetail::where([
-        //     'invoice_id' => $delivery_notes_detail->deliveryNote->invoice_id,
-        //     'product_id' => $delivery_notes_detail->product_id,
-        // ])->firstOrFail();
-        // }
+        $invoiceDetail = InvoiceDetail::where([
+            'invoice_id' => $delivery_notes_detail->deliveryNote->invoice_id,
+            'product_id' => $delivery_notes_detail->product_id,
+        ])->firstOrFail();
+        }
 
-        // $totalDeliveredQuantity = $this->getTotalDeliveredQuantity($totalDeliveryNoteDetails);
-        // $totalDeliveredQuantity = $quotationDetail->getDeliveredQuantity($quotationDetail);
-        // if(isset($totalDeliveredQuantity)){
-        //     $totalDeliveredQuantityExceptCurrentValue = $totalDeliveredQuantity - intval($delivery_notes_detail->delivered_quantity) ;
-        // }else{
-        //     $totalDeliveredQuantityExceptCurrentValue = 0;
-        // }
+        $totalDeliveredQuantity = $this->getTotalDeliveredQuantity($totalDeliveryNoteDetails);
+        $totalDeliveredQuantity = $quotationDetail->getDeliveredQuantity($quotationDetail);
+        if(isset($totalDeliveredQuantity)){
+            $totalDeliveredQuantityExceptCurrentValue = $totalDeliveredQuantity - intval($delivery_notes_detail->delivered_quantity) ;
+        }else{
+            $totalDeliveredQuantityExceptCurrentValue = 0;
+        }
 
-        // $data = [
-        //     "total_quantity" => $quotationDetail->quantity, //$totalQuantity =
-        //     // "total_delivered_quantity" => $totalDeliveredQuantity,
-        //     "total_delivered_quantity" => $totalDeliveredQuantityExceptCurrentValue,
-        //     "delivering_quantity" => $delivery_notes_detail->delivered_quantity,
-        //     "delivery_notes_detail" => $delivery_notes_detail,
-        //     "product" => array($delivery_notes_detail->product),
-        //     // "quotation" => $delivery_notes_detail->deliveryNote->quotation,
-        //     // "delivery_note" => $delivery_notes_detail->deliveryNote,
-        //     // "party" => $delivery_notes_detail->deliveryNote->quotation->party,
-        //     // 'balance_quantity' => $this->getBalanceQuantity($totalQuantity, $totalDeliveredQuantity), //not required anymore
-        // ];
+        $data = [
+            "total_quantity" => $quotationDetail->quantity, //$totalQuantity =
+            // "total_delivered_quantity" => $totalDeliveredQuantity,
+            "total_delivered_quantity" => $totalDeliveredQuantityExceptCurrentValue,
+            "delivering_quantity" => $delivery_notes_detail->delivered_quantity,
+            "delivery_notes_detail" => $delivery_notes_detail,
+            "product" => array($delivery_notes_detail->product),
+            // "quotation" => $delivery_notes_detail->deliveryNote->quotation,
+            // "delivery_note" => $delivery_notes_detail->deliveryNote,
+            // "party" => $delivery_notes_detail->deliveryNote->quotation->party,
+            // 'balance_quantity' => $this->getBalanceQuantity($totalQuantity, $totalDeliveredQuantity), //not required anymore
+        ];
 
         return [$delivery_notes_detail];
     }
