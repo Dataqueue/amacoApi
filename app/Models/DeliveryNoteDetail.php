b6b397d41ce3486a5705208f4627d5ac698a5706
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-01 12:41:27 +0530

    Update DeliveryNoteDetail.php

--- app/Models/DeliveryNoteDetail.php
+++ app/Models/DeliveryNoteDetail.php
@@ -36,7 +36,7 @@ class DeliveryNoteDetail extends Model
         //     return $totalDeliveryNoteDetail;
         // }
         // return 0;
-        $val;
+        return $val;
     }
 
     // there is no need for this
