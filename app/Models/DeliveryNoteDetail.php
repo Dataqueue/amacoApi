7cd5df258f658db71a291dfd222046244e491b81
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-02 16:44:22 +0530

    Update DeliveryNoteDetail.php

--- app/Models/DeliveryNoteDetail.php
+++ app/Models/DeliveryNoteDetail.php
@@ -48,7 +48,8 @@ class DeliveryNoteDetail extends Model
             'product_id' => $pid,
         ])->sum('delivered_quantity');
         $latest=DeliveryNoteDetail::where(['delivery_note_id'=>$id,'product_id'=>$pid])->orderBy('created_at','desc')->first('delivered_quantity');
-        return $totalDeliveryNoteDetails-(int)(isset($latest)?$latest->delivered_quantity:0);
+        // return $totalDeliveryNoteDetails-(int)(isset($latest)?$latest->delivered_quantity:0);
+        return $latest;
     }
 
     public function showDeliveredNoteDetail($id,$productId)
