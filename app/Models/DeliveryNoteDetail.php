b790f8a2af222f39542557f343bd925130c253f4
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-02 19:10:25 +0530

    Update DeliveryNoteDetail.php

--- app/Models/DeliveryNoteDetail.php
+++ app/Models/DeliveryNoteDetail.php
@@ -51,7 +51,7 @@ class DeliveryNoteDetail extends Model
         // return $totalDeliveryNoteDetails-(int)(isset($latest)?$latest->delivered_quantity:0);
         if($latest->isEmpty())
         {
-            return "deee".$totalDeliveryNoteDetails-(int)(0);
+            return "deee".$totalDeliveryNoteDetails;
         }
         else
         {
