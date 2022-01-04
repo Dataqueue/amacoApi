902c2ce5a7879717de6454b49c293b9771af6afa
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-04 06:51:53 +0530

    save

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -40,7 +40,7 @@ class AuthController extends Controller
             $count = DB::table('user_divisions')->join('divisions','divisions.id','user_divisions.div_id')->where(['user_divisions.u_id'=>Auth::user()->id,'divisions.id'=>3])->count();
             if($count>0)
             {
-                $type=2;
+                $type=1;
             }
             else {
                 $type=1;
