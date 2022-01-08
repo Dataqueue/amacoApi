782d3e39e07566e58462e45d10c250a82337a397
amacoerp <deekshitha.cse5235@gmail.com>
2022-01-08 15:58:33 +0530

    Update UserController.php

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,7 @@ use App\Models\PaymentAccount;
 use App\Models\UserDivision;
 use App\Models\Division;
 use App\Models\Role;
+use App\Models\PermissionDenied;
 use App\Models\Investment;
 use Illuminate\Http\Request;
 use App\Models\User;
